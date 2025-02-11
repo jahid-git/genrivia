1b6e7dbdd7c32dbe593af62cc6f5edf9d6c2f2ee
Jahid Hasan <jahidsite0@gmail.com>
2025-02-11 22:37:32 +0600

    Update middleware to redirect to the login URL based on the request URL

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,8 +4,9 @@ import { getToken } from 'next-auth/jwt';
 export default async function middleware(req: NextRequest) {
 	if (req.nextUrl.pathname === '/') {
 		const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+		console.log(token);
 		if (!token) {
-			return NextResponse.redirect(new URL('/login', 'https://genrivia.vercel.app'));
+			return NextResponse.redirect(new URL('/login', req.url));
 		}
 	}
 	return NextResponse.next();
