efdba61bd24c2abf2f7c6aa7f920b5723c56b884
Jahid Hasan <jahidsite0@gmail.com>
2025-03-14 18:19:59 +0600

    Update chat API endpoint and remove token logging in middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,7 +4,6 @@ import { getToken } from 'next-auth/jwt';
 export default async function middleware(req: NextRequest) {
 	if (req.nextUrl.pathname === '/') {
 		const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true });
-		console.log(token);
 		if (!token) {
 			return NextResponse.redirect(new URL('/login', req.url));
 		}
